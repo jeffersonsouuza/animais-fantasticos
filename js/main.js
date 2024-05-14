17b15f22df0d7244f61f2bb50b116c4ea7c774bc
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-05-14 16:03:07 -0300

    improve: adicionar navegacao por tab na seção animais

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
+
+if (tabMenu.length && tabConteudo.length) {
+  tabConteudo[0].classList.add("ativo");
+
+  function activeTab(index) {
+    tabConteudo.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabConteudo[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
