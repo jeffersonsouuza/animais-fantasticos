97164580eaba0bbcefcd5c2f402cb769766ef08e
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-05-28 20:24:31 -0300

    improve: implementar seção de números (animacao)

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -6,9 +6,12 @@ export default function initAnimacaoScroll() {
 
     function animaScroll() {
       sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-        if (sectionTop < 0) {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
           section.classList.add("ativo");
+        } else if (section.classList.contains("ativo")) {
+          section.classList.remove("ativo");
         }
       });
     }
