821fa29c565c3306a46261710f550d1bb7141e12
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-06-12 16:22:13 -0300

    improve: consumir dados dos animais direto de um arquivo json

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,37 @@
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function initFetchAnimais() {
+
+    const animaisNumeros = document.querySelector('.numeros-grid');
+    async function fetchAnimais(url) {
+        try {
+            const animaisResponse = await fetch(url);
+            const animaisJson = await animaisResponse.json();
+
+            animaisJson.forEach(animal => createAnimal(animal));
+
+            initAnimaNumeros();
+        } catch (erro) {
+            animaisNumeros.textContent = 'Ocorreu um problema ao verificar os dados dos animais';
+        }
+    }
+
+    function createAnimal(animal) {
+        const div = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const span = document.createElement('span');
+
+        div.classList.add('numero-animal');
+        span.setAttribute('data-numero', "");
+
+        h3.innerText = animal.specie;
+        span.innerText = animal.total;
+
+        animaisNumeros.appendChild(div);
+        div.append(h3, span);
+    }
+
+    fetchAnimais('./animaisapis.json');
+}
+
+
